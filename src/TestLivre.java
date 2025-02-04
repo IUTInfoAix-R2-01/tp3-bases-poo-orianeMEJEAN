1e0509e16d8d2d040dc236cb0d7daf9de0b1a303
oriane <oriane@oriane-VirtualBox>
2025-02-04 14:29:35 +0100

    AJout du test pour le setter

--- src/TestLivre.java
+++ src/TestLivre.java
@@ -4,11 +4,15 @@ public class TestLivre
 	public static void main (String[] args)
 	{
 		Livre l1 = new Livre ("Solo Leveling", 14.99, 608);
-		System.out.println(l1.toString());
-		
-		System.out.println(" ");
+		System.out.println(l1.toString() + "\n");
 		
 		Livre l2 = new Livre ("Le Labyrinthe", 28, 86);
-		System.out.println (l2.toString());
+		System.out.println (l2.toString() + "\n");
+		
+		l1.setQuantite(20);
+		System.out.println(l1 + "\n");
+		
+		l2.setQuantite(4);
+		System.out.println(l2 + "\n");
 	}
 }
