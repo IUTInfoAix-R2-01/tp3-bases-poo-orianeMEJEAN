a4739a10aef42588d37389c7c660e7fcc86a901f
oriane <oriane@oriane-VirtualBox>
2025-02-04 16:59:46 +0100

    Modification et test de la class Book de l'exercice du YAIP 2-1

--- src/TestBook.java
+++ src/TestBook.java
@@ -13,12 +13,22 @@ public class TestBook
 		d1.setQuantite (28);
 		System.out.println("name is: " + d1.getName());
 		System.out.println("price is: " + d1.getPrice());
-		System.out.println("qty is: " + d1.getQuantite() + "\n");
+		System.out.println("quantity is: " + d1.getQuantite() + "\n");
 		System.out.println("Author is: " + d1.getAuthor());
 		System.out.println("Author's name is: " + d1.getAuthor().getName());
 		System.out.println("Author's email is: " + d1.getAuthor().getEmail() + "\n");
 
-		Book anotherBook = new Book("more Java", 29.95, new Author("Paul Tan", "paul@somewhere.com", 'm'));
-		System.out.println(anotherBook);
+		Book otherBook = new Book("more Java", 29.95, new Author("Paul Tan", "paul@somewhere.com", 'm'));
+		System.out.println(otherBook + "\n");
+		
+		
+		Book aBook = new Book ("Le Labyrinthe", 28, new Author ("James Dashne", "jamesDashner@gmail.com", 'm'), 86);
+		System.out.println(aBook);
+		System.out.println("Author's name = " + aBook.getAuthor().getName());
+		System.out.println("Author's email = " + aBook.getAuthor().getEmail());
+		
+		
+		
+		
 	}
 }
