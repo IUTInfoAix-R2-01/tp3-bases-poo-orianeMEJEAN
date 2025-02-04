67550cb882b1470d90148566d56ec2d2c738cfd7
oriane <oriane@oriane-VirtualBox>
2025-02-04 16:44:46 +0100

    2ème class de l'exercice du YAIP 2-1

---
+++ src/Book.java
@@ -0,0 +1,59 @@
+
+
+public class Book 
+{
+	private String name;
+	private Author author;
+	private double price;
+	private int quantite;
+	
+	public Book(String n, double p, Author au)
+	{
+		this.name = n;
+		this.price = p;
+		this.author = au;
+	}
+	
+	public Book(String n, double p, Author au, int q)
+	{
+		this.name = n;
+		this.price = p;
+		this.author = au;
+		this.quantite = q;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public Author getAuthor()
+	{
+		return author;
+	}
+	
+	public double getPrice()
+	{
+		return price;
+	}
+	
+	public int getQuantite()
+	{
+		return quantite;
+	}
+	
+	public void setPrice(double p)
+	{
+		this.price = p;
+	}
+	
+	public void setQuantite(int quantite)
+	{
+		this.quantite = quantite;
+	}
+	
+	public String toString()
+	{
+		return "Book [nom = " + name + ", " + author + ", prix = " + price + "€, quantité = " + quantite + "]";
+	}
+}
